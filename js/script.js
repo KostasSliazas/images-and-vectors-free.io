f12030b058bb05c279a338f7adc9a808bbb1a4ea
Kostas <kostas.sliazas@gmail.com>
2023-02-08 23:13:05 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.getElementsByTagName('header')[0]
 
 
-const body = ()=> document.getElementsByTagName('body')[0].style.overflow = header.getAttribute('class') === 'active'?'hidden' : 'auto'
+const body = ()=> document.body.style.overflow = header.getAttribute('class') === 'active'?'hidden' : 'auto'
 document.addEventListener('click', function (e) {
     if (e.target.id === "menu" && header.getAttribute('class') !== 'active')
       return header.setAttribute('class', 'active') || body()
